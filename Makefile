da0a484da9d9bf70c93312c624e95c250fb78c4b
Stan <sdbondi@gmail.com>
2014-01-15 21:54:24 +0200

    Fixed build errors

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ all: build
 
 build:
 	docker build -t fixate/ruby-rbx .
+
+push:
+	docker build -t fixate/ruby-rbx
--- install_rubinius.sh
+++ install_rubinius.sh
@@ -7,20 +7,18 @@ TAR_FILE=rubinius-${TAG}.tar.bz2
 
 cd /tmp
 wget http://releases.rubini.us/$TAR_FILE
-tar xzf $TAR_FILE
+tar xjf $TAR_FILE
 
 cd ${TAR_FILE%.*.*}
 
-curl -fsSL curl.haxx.se/ca/cacert.pem -o /usr/local/src/rubinius-$TAG/lib/rubygems/ssl_certs/cert.pem
 bundle
 ./configure --prefix=$PREFIX
 rake install
 
-rm -rf ${TAR_FILE} ${TAR_FILE%.*.*}
+rm -rf ${TAR_FILE} ${TAR_FILE%.*.*}/
 
+export PATH=/usr/local/rubinius/bin:$PATH
 locale-gen en_US.UTF-8
 update-locale LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-echo "export LC_ALL=en_US.UTF-8" >> /etc/environment
 /usr/local/rubinius/bin/gem install bundler
 /usr/local/rubinius/bin/gem install rubysl-tracer
